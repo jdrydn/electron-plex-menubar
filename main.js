547de0d94c5d17f68712a8ebabb5b5f2bc6d889c
jdrydn <james@jdrydn.com>
2023-02-14 12:21:36 +0000

    Added secret support for Disney+ & Netflix
    Not that Netflix will be needed - they're doing a great job self-destructing as-is!

--- main.js
+++ main.js
@@ -1,21 +1,55 @@
+const { app, Menu } = require('electron');
 const { menubar } = require('menubar');
 
-const plex = menubar({
-  index: 'https://app.plex.tv/desktop',
-  tooltip: 'Plex',
-  browserWindow: {
-    height: 250,
-    width: 400,
-    alwaysOnTop: true,
-    webPreferences: {
-      nodeIntegration: false,
-      zoomFactor: 0.5,
-    },
-  },
-  showOnAllWorkspaces: true,
-  windowPosition: 'trayLeft',
-});
+const urls = [
+  { label: 'Plex', url: 'https://app.plex.tv/desktop' },
+  { label: 'Netflix', url: 'https://www.netflix.com/browse' },
+  { label: 'Disney+', url: 'https://www.disneyplus.com/home' },
+];
 
-plex.on('ready', function () {
-  console.log('Webview is ready');
+app.on('ready', () => {
+  let currentUrl = urls[0].url;
+
+  const mb = menubar({
+    index: currentUrl,
+    tooltip: 'Plex',
+    browserWindow: {
+      height: 250,
+      width: 400,
+      alwaysOnTop: true,
+      webPreferences: {
+        nodeIntegration: false,
+        zoomFactor: 0.5,
+      },
+    },
+    preloadWindow: true,
+    showOnAllWorkspaces: true,
+    showDockIcon: true,
+    windowPosition: 'trayLeft',
+  });
+
+  function rebuildMenu() {
+    return Menu.buildFromTemplate([
+      {
+        label: 'Change Source',
+        submenu: urls.map(({ label, url }) => ({
+          label,
+          enabled: currentUrl !== url,
+          click: () => {
+            currentUrl = url;
+            mb.window.loadURL(url);
+            app.dock.setMenu(rebuildMenu());
+          },
+        })),
+      },
+    ]);
+  }
+
+  mb.on('ready', function () {
+    console.log('Webview is ready');
+
+    if (process.platform === 'darwin') {
+      app.dock.setMenu(rebuildMenu());
+    }
+  });
 });
